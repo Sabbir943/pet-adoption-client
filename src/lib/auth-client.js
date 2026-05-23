2c4b1c673afee0af30fbcf2fc83b6b7c64683059
sabbir--ahmad <mdsabbirahmad943@gmail.com>
2026-05-23 23:22:09 +0600

    added

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,10 +1,10 @@
 import { createAuthClient } from "better-auth/react"
-import { jwtClient } from "better-auth/client/plugins"
+// import { jwtClient } from "better-auth/client/plugins"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     baseURL:process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
-      plugins: [
-    jwtClient() 
-  ]
+  //     plugins: [
+  //   jwtClient() 
+  // ]
 })
 export const { signIn, signUp, useSession } = createAuthClient()
